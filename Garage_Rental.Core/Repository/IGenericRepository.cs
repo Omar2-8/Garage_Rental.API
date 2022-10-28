f79af8a3330a5ac1a0f5baa17bcd7db4306defe9
falahkh7 <falah.amer079@gmail.com>
2022-10-28 19:10:29 +0200

    Adding Generic Repository

---
+++ Garage_Rental.Core/Repository/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garage_Rental.Core.Repository
+{
+    public interface IGenericRepository<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        bool Create(T t);
+        void Update(T t);
+        void Delete(int id);
+    }
+}
---
+++ Garage_Rental.Infra/Repository/Users.cs
@@ -0,0 +1,35 @@
+﻿using Garage_Rental.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garage_Rental.Infra.Repository
+{
+    internal class Users : IGenericRepository<Users>
+    {
+        public bool Create(Users t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Users> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Users GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Users t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
