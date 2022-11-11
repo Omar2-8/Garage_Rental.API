16bc728f09e97dc134b17c260896a1734f0fd4c9
Baron2001 <alijaara2001@gmail.com>
2022-11-11 22:31:16 +0300

    new

--- FrontEnd/GarageRentalFrontEnd/src/app/auth/auth-routing.module.ts
+++ FrontEnd/GarageRentalFrontEnd/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,8 @@
 import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
