d8494233afeb6a280a18713bced4d6f5259a9337
Omar AL-Ghouti <algohti23@gmail.com>
2022-12-08 23:14:04 +0300

    add index to users table in Admin

--- FrontEnd/GarageRentalFrontEnd/src/app/admin/user-details/user-details.component.ts
+++ FrontEnd/GarageRentalFrontEnd/src/app/admin/user-details/user-details.component.ts
@@ -6,28 +6,53 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+  styleUrls: ['./user-details.component.css'],
 })
 export class UserDetailsComponent implements OnInit {
+  first = 0;
 
-  constructor(public admin:AdminService,public users:UserService,public dialog: MatDialog) { }
-  @ViewChild('callDeleteDailog') callDelete!:TemplateRef<any>
+  rows = 10;
+
+  constructor(
+    public admin: AdminService,
+    public users: UserService,
+    public dialog: MatDialog
+  ) {}
+  @ViewChild('callDeleteDailog') callDelete!: TemplateRef<any>;
   ngOnInit(): void {
     this.admin.getAllUsers();
   }
-  openDeleteDailog(id:number)
-  {
-    const dialogRef=  this.dialog.open(this.callDelete);
-    dialogRef.afterClosed().subscribe((result)=>{
-      if(result!=undefined)
-      {
-        if(result=='yes')
-        {
+  openDeleteDailog(id: number) {
+    const dialogRef = this.dialog.open(this.callDelete);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result != undefined) {
+        if (result == 'yes') {
           this.users.deleteUser(id);
-        }
-          else if(result=='no')
-          console.log('thank you '); 
+        } else if (result == 'no') console.log('thank you ');
       }
-    })
+    });
+  }
+
+  next() {
+    this.first = this.first + this.rows;
+  }
+
+  prev() {
+    this.first = this.first - this.rows;
+  }
+
+  reset() {
+    this.first = 0;
+  }
+
+  isLastPage(): boolean {
+    return this.admin.user
+      ? this.first === this.admin.user.length - this.rows
+      : true;
+  }
+
+  isFirstPage(): boolean {
+    return this.admin.user ? this.first === 0 : true;
   }
 }
