cb7043340856bb40c0085a3540dd2a64a3e8ac6a
falahkh7 <falah.amer079@gmail.com>
2022-12-12 20:41:52 +0300

    Rent fixed

--- FrontEnd/GarageRentalFrontEnd/src/app/Components/rent-garage/rent-garage.component.ts
+++ FrontEnd/GarageRentalFrontEnd/src/app/Components/rent-garage/rent-garage.component.ts
@@ -16,9 +16,10 @@ export class RentGarageComponent implements OnInit {
   @ViewChild('callDeleteDailog') callDelete!:TemplateRef<any>;
   @ViewChild('callCreateDailog') callCreateRent!: TemplateRef<any>;
   @ViewChild('callVisaDailog') callVisaRent!: TemplateRef<any>;
+  @ViewChild('callCarDailog') callCarRent!: TemplateRef<any>;
   garageid :any;
   userid:any;
-  visA_NUMBER:Visa={
+  viR:Visa={
     visA_ID: 0,
     visA_NAME: '',
     visA_NUMBER: 0,
@@ -45,9 +46,22 @@ export class RentGarageComponent implements OnInit {
   });
   ChangeAmount: FormGroup = new FormGroup({
     
-    visA_NUMBER:new FormControl(''),
+    visA_NUMBER:new FormControl('',Validators.required),
     visA_AMOUNT: new FormControl('', Validators.required),
   });
+  CheckVisa: FormGroup = new FormGroup({
+    
+    visA_NUMBER:new FormControl('',Validators.required),
+    cvC_CVV: new FormControl('', Validators.required),
+  });
+  CheckCar: FormGroup = new FormGroup({
+    
+    
+    caR_TYPE: new FormControl('', Validators.required),
+    caR_PLATE: new FormControl('', Validators.required),
+  });
+
+
   p_data_c: any = {};
 
 
@@ -70,12 +84,40 @@ export class RentGarageComponent implements OnInit {
     this.dialog.open(this.callVisaRent);
     this.userService.getAllVisa();
   }
-  Visa(num:number) {
+  visa_t:any;
+  
+  Visa() {
+  
     debugger;
-    this.userService.getAllVisa();
+    let num:number=this.CheckVisa.value.visA_NUMBER;
+    let cvc:number=this.CheckVisa.value.cvC_CVV;
+    if(num!=0 &&cvc!=0){
+  
+   this.visa_t= this.userService.visa.filter(x=>x.visA_NUMBER==num &&x.cvC_CVV==cvc);
+  
+   if(this.visa_t.length!=0){
+   this.toastr.success('The visa has been selected! Visa Number is :'+num);}
+   else
+   {
+    this.toastr.error('The visa entered is incorrect!');
+   }
+   }else{
+    this.toastr.error('Please Add Visa For rent!');
+   }
+
+    
   }
+  openCarDailog() {
+    this.dialog.open(this.callVisaRent);
+    this.userService.getListCarId(this.userid);
+  }
+  temb_car:any;
   Car() {
+    
+    let cType:string=this.CheckCar.value.caR_TYPE;
+    let cPlate:string=this.CheckCar.value.caR_TYPE;
     this.userService.getCarId(this.userService.car2.caR_ID);
+    this.temb_car= this.userService.car.filter(x=>x.caR_TYPE==cType&&x.caR_PLATE==cPlate);
   }
 
   saveData() {
@@ -86,9 +128,10 @@ export class RentGarageComponent implements OnInit {
     if(this.userService.garage.garagE_MODE !='unavailable'){
       if(this.userService.garage.availablE_FROM <=this.createForm.value.starT_TIME && this.userService.garage.availablE_TO >=this.createForm.value.enD_TIME ){
       
-        if( this.visA_NUMBER.visA_AMOUNT!=0){
-        if(this.userService.visa.visA_AMOUNT>this.userService.garage.renT_PRICE*(this.createForm.value.enD_TIME-this.createForm.value.starT_TIME)){
-          this.ChangeAmount.value.visA_AMOUNT=this.userService.visa.visA_AMOUNT-this.userService.garage.renT_PRICE*(this.createForm.value.enD_TIME-this.createForm.value.starT_TIME);
+        if( this.visa_t[0].visA_AMOUNT!=0){
+        if(this.visa_t[0].visA_AMOUNT>this.userService.garage.renT_PRICE*(this.createForm.value.enD_TIME-this.createForm.value.starT_TIME)){
+          this.ChangeAmount.value.visA_NUMBER=this.visa_t[0].visA_NUMBER;
+          this.ChangeAmount.value.visA_AMOUNT=this.visa_t[0].visA_AMOUNT-this.userService.garage.renT_PRICE*(this.createForm.value.enD_TIME-this.createForm.value.starT_TIME);
           
           this.userService.ChangeAmountVisa(this.ChangeAmount.value)
           this.userService.createRent(this.createForm.value);}
